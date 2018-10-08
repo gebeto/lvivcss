e2bae875952a61efc1377e5eff0c6f1f8cd7b34e
Babich Sergiy <babich.ss@gmail.com>
2018-10-08 21:54:55 +0300

    Update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HeaderComponent {
+  readonly notTicketsRoute$: Observable<boolean> = this.router.events
+    .pipe(
+      filter(e => e instanceof NavigationEnd),
+      map((e: NavigationEnd) => e.url !== '/tickets')
+    );
+  constructor(
+    readonly router: Router
+  ) { }
 }
