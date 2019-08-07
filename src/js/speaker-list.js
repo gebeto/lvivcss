c21437c5daf82014be12362e9c5191e341441257
Babich Sergiy <babich.ss@gmail.com>
2019-08-07 14:18:27 +0300

    Structure revamp

--- src/js/speaker-list.js
+++ src/js/speaker-list.js
@@ -1,22 +1,28 @@
 const script = document.currentScript;
 const template = document.getElementById('speakerCardTemplate').cloneNode(true);
+const textContentKeys = ['speakerName', 'speakerBio', 'talkName', 'talkDescription'];
+
 async function getSpeakers() {
-	const speakers = await ((await fetch('./speakers.json')).json());
+	const speakers = await ((await fetch('../data/speakers.json')).json());
 	const buffer = document.createDocumentFragment();
 
 	speakers.forEach(speaker => {
 		const node = document.importNode(template.content, true);
-		node.querySelector('[data-role="talkName"]').textContent = speaker.talkName;
-		node.querySelector('[data-role="speakerName"]').textContent = speaker.speakerName;
-		node.querySelector('[data-role="speakerBio"]').textContent = speaker.speakerBio;
-		node.querySelector('[data-role="talkDescription"]').textContent = speaker.talkDescription;
+
+		for (key of textContentKeys) {
+			node.querySelector(`[data-role="${key}"]`).textContent = speaker[key];
+		}
+
 		const img = node.querySelector('[data-role="speakerPhoto"]');
+
 		img.src = `/assets/images/speakers/${speaker.image}.png`;
 		img.alt = speaker.speakerName;
+
 		buffer.appendChild(node);
 	});
 
 	document.getElementById('speakers').appendChild(buffer);
 	document.head.removeChild(script);
 }
+
 getSpeakers();
