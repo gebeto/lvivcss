8f64a4de1db88f8452ca63492b573fbf85c7fc6a
Babich Sergiy <babich.ss@gmail.com>
2019-08-19 08:50:34 +0300

    Use handlebars

---
+++ hbs/helpers/eq.js
@@ -0,0 +1,15 @@
+var equal = function() {};
+
+equal.register = function(Handlebars) {
+	Handlebars.registerHelper('eq', function(lvalue, rvalue, options) {
+		if (arguments.length < 3)
+			throw new Error("Handlebars Helper equal needs 2 parameters");
+		if (lvalue != rvalue) {
+			return options.inverse(this);
+		} else {
+			return options.fn(this);
+		}
+	});
+};
+
+module.exports = equal;
