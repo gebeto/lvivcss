a6851650b694a751c231382e63b7e2740e3c14ba
Babich Sergiy <babich.ss@gmail.com>
2019-08-23 13:39:35 +0300

    .

--- js/notifications.js
+++ js/notifications.js
@@ -1,7 +1,7 @@
 Notification.requestPermission().then(function(permission) {
-	alert(permission);
+	// alert(permission);
 	new Notification(`LvivCSS'19`, {
-		image: '/assets/images/speakers/shpilevska.jpg',
+		// image: '/assets/images/speakers/shpilevska.jpg',
 		body: `13:00 "Ніч темна і повна анти-pattern'ів"
 Олена Шпілевська`,
 		icon: '/assets/favicon.png'
