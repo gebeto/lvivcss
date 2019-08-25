f10f3a6f3db76abbbdb1d550276a6dec6e3ee3b2
Babich Sergiy <babich.ss@gmail.com>
2019-08-25 10:18:49 +0300

    Delete test

--- @test/js/utils.js
+++
@@ -1,4 +0,0 @@
-const appKey = 'lvivcssapp';
-
-export const getLocalState = () => JSON.parse(localStorage.getItem(appKey)) || {};
-export const setLocalState = (state) => localStorage.setItem(appKey, JSON.stringify(state));
