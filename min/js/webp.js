ee7e45742fc450dc3df242443d4f997ce97c1f8b
Sergiy Babich <babich.ss@outlook.com>
2020-02-16 10:08:40 +0200

    add webp detection

---
+++ min/js/webp.js
@@ -0,0 +1,1 @@
+const webP=new Image();webP.onerror=({type})=>document.documentElement.classList.add('no-webp');webP.src='data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
